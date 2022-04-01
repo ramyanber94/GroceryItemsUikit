f5982811b7bb32786c637c8b2087e57ad0304015
ramy <ramyanber@gmail.com>
2022-03-31 19:53:27 -0700

    Initial Commit

---
+++ GroceryItemsUikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroceryItemsUikit
+//
+//  Created by Ramy Anber on 2022-03-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
